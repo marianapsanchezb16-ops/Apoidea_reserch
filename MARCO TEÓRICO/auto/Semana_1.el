0e385ff42abca78d5cba38fb099ff35ebbc67ca6
Rafael Miranda-Esquivel <dmirandae@gmail.com>
2026-01-30 22:25:49 -0500

    semana 1 biblatex

---
+++ MARCO TEÓRICO/auto/Semana_1.el
@@ -0,0 +1,13 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "Semana_1"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "MORALES-ALBA2022"
+    "Rosado-Martínez_Santos-Murgas_Cobos-Hernández_Carrero_Flórez-Villamizar_2024"
+    "rasmussen2024taxonomic"
+    "marconi2022dna"
+    "alonso2024first"))
+ '(or :bibtex :latex))
+
